9c6b4301467663536f2a6df91b3ce479fbbbe57d
tatsuya.fujiwara <tatsuya.fujiwara01@gmail.com>
2018-10-07 14:25:30 +0000

    add column

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,3 +2,25 @@ class Task < ApplicationRecord
   validates :content, presence: true, length: { maxium: 10}
   validates :status, presence: true, length: { maximum: 10 }
 end
+
+<% if message.errors.any? %>
+
+    <div id="error_explanation">
+      <h2>
+<%= pluralize(message.errors.count, "error") %>
+ prohibited this message from being saved:</h2>
+
+      <ul>
+        
+<% message.errors.full_messages.each do |message| %>
+
+          <li>
+<%= message %>
+</li>
+        
+<% end %>
+
+      </ul>
+    </div>
+  
+<% end %>
